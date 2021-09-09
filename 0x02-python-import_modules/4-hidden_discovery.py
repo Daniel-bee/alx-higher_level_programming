3be74a511fbfe05bd9572e874df4873f73cd98a1
Daniel-bee <daniel123_86@live.com>
2021-09-09 15:41:01 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
 name_list = dir(hidden_4)
+
 if __name__ == "__main__":
-    for name in name_list:
-        if name[:2] != "__":
-            print("{}".format(name))
+    for names in name_list:
+        if names[:2] != "__":
+            print("{}".format(names))
