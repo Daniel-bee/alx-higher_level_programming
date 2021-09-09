34f1d18473e32ded502cd7f6d3b1022b3d2a09bc
Daniel-bee <daniel123_86@live.com>
2021-09-09 14:30:02 +0300

    arg count and print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if arg_len == 1:
         print("{0 arguments.")
     elif arg_len == 2:
-        print("{1 argument.")
+        print("{1 argument:")
         print("{}: {}".format(arg_len - 1, sys.argv[1]))
     else:
         print("{} arguments:".format(arg_len - 1))
