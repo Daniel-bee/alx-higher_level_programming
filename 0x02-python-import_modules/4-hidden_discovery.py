8c9eafe92f40408b22f260a6747a18b032347844
Daniel-bee <daniel123_86@live.com>
2021-09-09 15:24:32 +0300

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,6 @@ from hidden_4 import *
 name_list = dir(hidden_4)
 if __name__ == "__main__":
     for name in name_list:
-        print(name)
+        if name[:2] != '__':
+            print(name)
 
