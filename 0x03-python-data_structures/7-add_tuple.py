b9fd0ede2c9ce4b13a841fa0c86e7e4f255c9044
Daniel-bee <daniel123_86@live.com>
2021-09-13 13:39:27 +0300

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,5 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    sum_ = tuple(map(lambda a, b: a + b, tuple_a, tuple_b))
+    sum_ = tuple(map(sum, zip(tuple_a, tuple_b)))
     return sum_
