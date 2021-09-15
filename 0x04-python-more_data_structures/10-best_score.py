7ad995b18e72efd7f2b7608fc84a5879839f42cc
Daniel-bee <daniel123_86@live.com>
2021-09-15 16:20:05 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
+    """Returns a key with the biggest integer value."""
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    ret, bigt = list(a_dictionary.keys())[0], a_dictionary[ret]
-    for key, value in a_dictionary.items():
-        if value > bigt:
-            ret, bigt = key, value
-
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for key, value in sorted( a_dictionary.items()):
+        if value > big:
+            big = value
+            ret = key
     return (ret)
-
