cda946a80221a1528dacdbad5a91cedf55330eec
Daniel-bee <daniel123_86@live.com>
2021-09-15 16:15:47 +0300

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return max(a_dictionary)
+
+    ret, bigt = list(a_dictionary.keys())[0], a_dictionary[ret]
+    for key, value in a_dictionary.items():
+        if value > big:
+            ret, bigt = key, value
+
+    return (ret)
+
