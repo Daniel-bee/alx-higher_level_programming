077ea0be9379034a9bfba7c2afc9eee4c8129329
Daniel-bee <daniel123_86@live.com>
2021-11-16 13:47:57 +0300

    crete table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
 -- script that creates a table called
 -- first_table in the current database
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
+CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256),
+	hbtn_0c_0
 	) ENGINE=INNODB;
