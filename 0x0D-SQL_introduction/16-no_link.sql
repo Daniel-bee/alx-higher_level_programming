54de53bc59f257258d2a562a53a7f2a9def979f5
Daniel-bee <daniel123_86@live.com>
2021-11-17 16:26:49 +0300

    list

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,2 @@
 -- list
-UPDATE second_table SET score = 12, name = 'Aria' WHERE name = 'George';
-INSERT INTO second_table (id, name, score) VALUES (2, "Aria", 18);
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE NAME NOT NULL ORDER BY score DESC;
