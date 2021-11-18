3c2a673c8af2b129d633627df1ce567e3f118198
Daniel-bee <daniel123_86@live.com>
2021-11-18 16:42:35 +0300

    create table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,13 +2,11 @@
 CREATE DATABASE
 IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_0d_usa;
-
 CREATE TABLE
-IF NOT EXISTS cities
+IF NOT EXISTS hbtn_0d_usa.cities
 (id INT UNIQUE NOT NULL AUTO_INCREMENT,
  state_id INT NOT NULL,
  name VARCHAR(256) NOT NULL,
- PRIMARY KEY (id)
- FOREIGN KEY (id) REFERENCES states(id)
+ PRIMARY KEY (id),
+ FOREIGN KEY (id) REFERENCES hbtn_0d_usa.states(id)
 );
