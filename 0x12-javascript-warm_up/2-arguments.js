ba5681b195fc48ce64ac905539619954ad151fcd
Daniel-bee <daniel123_86@live.com>
2021-12-13 19:33:39 +0300

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv;
+let count;
+
+args.forEach((val, index) => {
+  count = index;
+});
+if (count >= 2) {
+  console.log('Argument found');
+}
+if (count === 1) {
+  console.log('No argument');
+}
