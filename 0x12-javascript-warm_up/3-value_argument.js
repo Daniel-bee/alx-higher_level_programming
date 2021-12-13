3eccf8f89aa7a5ee87d490fb36bd7c0c652336a7
Daniel-bee <daniel123_86@live.com>
2021-12-13 20:30:40 +0300

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
 
 const args = process.argv;
-let count;
 
-args.forEach((val, index) => {
-  count = index;
-});
-
-if (count === 1) {
+if (!args[2]) {
   console.log('No argument');
 } else {
-  for (let i = 2; i <= count; i++) {
-    console.log(args[i]);
-  }
+  console.log(args[2]);
 }
