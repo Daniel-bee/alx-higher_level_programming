07b37f66f58cec69597ddbe9bb63a5bb9efd1fa0
Daniel-bee <daniel123_86@live.com>
2021-12-18 13:11:38 +0300

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,13 +15,19 @@ if __name__ == '__main__':
                          db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities \
-            INNER JOIN states ON cities.id = states.id \
+            INNER JOIN states ON cities.state_id = states.id \
             WHERE states.name  = %s \
             ORDER BY cities.id ASC", [sys.argv[4]])
     cities = cur.fetchall()
-
+    length = len(cities)
+    spc = ', '
+    count = 0
     for city in cities:
-        print(city[0], end=', ')
+        if count < length - 1:
+            print(city[0]+spc, end='')
+        else:
+            print(city[0], end='')
+        count +=1
     print()
 
     cur.close()
