0f25eed836f14bc74c203372a4f518ac0cb64d5a
Daniel-bee <daniel123_86@live.com>
2022-01-09 18:40:25 +0300

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         page = response.read()
