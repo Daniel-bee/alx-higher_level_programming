a3d0303d20bf7b22c325c8b559f2b27bf2312899
Daniel-bee <daniel123_86@live.com>
2022-01-09 23:17:12 +0300

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,12 @@
 """This module fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
-    s = url.read()
-    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(req.format(type(s), s, s.decode('utf-8')))
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        page = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
