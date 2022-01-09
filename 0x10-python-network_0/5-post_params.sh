0733159b1d413b757dd7c3c9e971319063381ba6
Daniel-bee <daniel123_86@live.com>
2022-01-09 11:33:31 +0300

    post

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # post
-curl -s $1 -X POST -d  "email=Daniel123_86@live.com&subject=I will always be here for PLD"
+curl -sX POST -d  "email=test@gmail.com&subject=I will always be here for PLD" $1
