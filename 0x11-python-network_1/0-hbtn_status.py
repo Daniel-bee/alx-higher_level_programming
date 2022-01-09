8a8572ec4ed7f756837ef19025b1b38327b8b252
Daniel-bee <daniel123_86@live.com>
2022-01-09 18:47:54 +0300

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         page = response.read()
         print("Body response:")
-        print(f"    - type: {type(page)}")
-        print(f"    - content: {page}")
-        print(f"    - utf8 content: {page.decode('utf8')}")
+        print("    - type: {}".format(type(page)))
+        print("    - content: {}".format(page))
+        print("    - utf8 content: {}".format(page.decode('utf8')))
