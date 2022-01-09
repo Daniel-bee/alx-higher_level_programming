b4690c82d59d6436cfec157b63cc10709e5b5b01
Daniel-bee <daniel123_86@live.com>
2022-01-09 22:44:23 +0300

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,16 @@
 sends a request to the URL and displays the body of the response
 """
 import sys
-import urllib.parse
+import urllib.error
 import urllib.request
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
 
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
