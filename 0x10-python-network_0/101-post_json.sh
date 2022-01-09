2c3a7ace5d63d017a6928938d9e67b5fa9057bd4
Daniel-bee <daniel123_86@live.com>
2022-01-09 14:24:22 +0300

    post json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # JSON POST request to a URL passed as the first argument
-curl -sX  POST -H "Content-Type: application/json" -d @$s2 $1
+curl -sX  POST -H "Content-Type: application/json" -d "$(cat "$2")" $1
