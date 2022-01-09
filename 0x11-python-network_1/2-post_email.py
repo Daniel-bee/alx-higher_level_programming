e9634af283a3dde1e6cc9b76eb985c9ee531fae4
Daniel-bee <daniel123_86@live.com>
2022-01-09 22:36:49 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-
+sends a request to the URL and displays the body of the response
 """
+import sys
+import urllib.parse
 import urllib.request
-from sys import argv
+
 
 if __name__ == "__main__":
-    val = {'email': argv[2]}
-    data = urllib.parse.urlencode(val)
-    data = data.encode('ascii')
-    req = urllib.request.Request(argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        page = response.read()
-        print(page.decode('utf-8'))
+
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
