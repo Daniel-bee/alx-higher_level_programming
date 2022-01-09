efcf381e5d7d87403291988d62163dae756bfa04
Daniel-bee <daniel123_86@live.com>
2022-01-09 23:01:56 +0300

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,8 @@ import requests
 
 
 if __name__ == "__main__":
-
+    req = requests.get(sys.argv[1]
     if requests.get(sys.argv[1].status_code >= 400:
-        print("Error code: {}".format(requests.get(\
-                sys.argv[1].status_code))
+        print("Error code: {}".format(req.status_code))
     else:
         print(requests.get(sys.argv[1].text)
