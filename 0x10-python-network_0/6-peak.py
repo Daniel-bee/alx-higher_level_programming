93a39cc1d194ac00db901ef7d45e8a016fccb5c6
Daniel-bee <daniel123_86@live.com>
2022-01-09 12:03:31 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-#Find a peak
+"""Find a peak"""
+
 def find_peak(list_of_integers):
     """return the peak"""
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort(reverse = True)
+        temp = list_of_integers[0]
+        return temp
