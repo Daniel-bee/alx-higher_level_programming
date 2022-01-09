ee5d39a0fd17a9d492e54ea5e6e9809f7ed74e47
Daniel-bee <daniel123_86@live.com>
2022-01-09 15:49:13 +0300

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me
-curl -sLH "Origin: You got me!" -X PUT 0.0.0.0:5000/catch_me
+curl "0.0.0.0:5000/catch_me" -sLX PUT -H "Origin: You got me!" 
