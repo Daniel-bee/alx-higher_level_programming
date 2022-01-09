337ce79f6eb7806cc725f62001f718f52ee84afe
Daniel-bee <daniel123_86@live.com>
2022-01-09 18:29:40 +0300

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+    page = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(page)}")
+    print(f"\t- content: {page}")
+    print(f"\t- utf8 content: {page.decode('utf8')}")
