601f2d86e4e145abc0d15eef73f65d1c4ed77bd0
Daniel-bee <daniel123_86@live.com>
2022-02-01 19:26:55 +0300

    request store

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const options = {
+  url: process.argv[2],
+  method: 'GET',
+  json: true
+};
+const fs = require('fs');
+request(options, (error, response, body) => {
+  if (error) console.log(error);
+  fs.writeFile(process.argv[3], body, 'utf-8', err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
