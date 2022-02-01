ef5aeaa14266e3a8e936e4c38b24267dc2070f73
Daniel-bee <daniel123_86@live.com>
2022-02-01 15:39:26 +0300

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
+/*
 const options = {
   url: process.argv[2],
   method: 'GET'
 };
-request(options, (error, response, body) => {
+*/
+request(process.argv[2], (error, response, body) => {
   if (error) console.log(error);
   console.log('Code:', response.statusCode);
 });
