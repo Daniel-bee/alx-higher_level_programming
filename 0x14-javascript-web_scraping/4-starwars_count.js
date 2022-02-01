8bf94f4d99e14e8cc9127f75764914c7e53ba49a
Daniel-bee <daniel123_86@live.com>
2022-02-01 18:21:38 +0300

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,11 +2,19 @@
 
 const request = require('request');
 const options = {
-  url: process.argv[2] + '/18/',
-  method: 'GET'
+  url: process.argv[2],
+  method: 'GET',
+  json: true
 };
 
 request(options, (error, response, body) => {
   if (error) console.log(error);
-  console.log(body.length);
+  let count = 0;
+  body.results.forEach(function (data) {
+    const len = data.characters.length;
+    for (let i = 0; i < len; i++) {
+      if (data.characters[i] === 'https://swapi-api.hbtn.io/api/people/18/') count += 1;
+    }
+  });
+  console.log(count);
 });
