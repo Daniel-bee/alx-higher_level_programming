c8d263067c289293103f6c09eb864f2aa306d0cc
Daniel-bee <daniel123_86@live.com>
2022-02-01 21:59:36 +0300

    completed task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-let resultList = [];
-let i = 0;
-const countDict = {};
-let key = '';
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    resultList = JSON.parse(body);
-    for (i = 0; i < resultList.length; i++) {
-      key = resultList[i].userId;
-      if (resultList[i].completed === true) {
-        if (!countDict[key]) {
-          countDict[key] = 1;
-        } else {
-          countDict[key] += 1;
-        }
-      }
+const request = require('request');
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+request(options, (error, response, body) => {
+  if (error) console.log(error);
+  const obj = JSON.parse(body);
+  let firstId = obj[0].userId;
+  const json = {};
+  function count (id) {
+    let count = 0;
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i].completed && id === obj[i].userId) count++;
     }
-    console.log(countDict);
+    return count;
   }
+  json[firstId] = count(firstId);
+  for (let i = 1; i < obj.length; i++) {
+    if (firstId !== obj[i].userId) {
+      json[obj[i].userId] = count(obj[i].userId);
+      firstId = obj[i].userId;
+    }
+  }
+  console.log(json);
 });
