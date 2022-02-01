a57ce27e4f68a9b0c884a7f3de346ce6e9ced538
Daniel-bee <daniel123_86@live.com>
2022-02-01 14:17:28 +0300

    write on file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-/*
-script that writes a string to a file
-process.argv[2] -> file name
-process.argv[3] -> file content
-*/
+
 fs.writeFile(process.argv[2], process.argv[3] + '\n', 'utf-8', err => {
   if (err) {
     console.error(err);
