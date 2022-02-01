be184a508ef996e153b1c2afd91d56ddc5deca5d
Daniel-bee <daniel123_86@live.com>
2022-02-01 22:32:33 +0300

    completed task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,25 +6,14 @@ const options = {
 };
 request(options, (error, response, body) => {
   if (error) console.log(error);
-  const obj = JSON.parse(body);
-  let firstId = obj[0].userId;
-  const json = {};
-  function count (id) {
-    let count = 0;
-    for (let i = 0; i < obj.length; i++) {
-      if (obj[i].completed && id === obj[i].userId) count++;
-    }
-    return count;
-  }
-  let key = '';
-  key = firstId;
-  json[firstId] = parseInt(count(firstId));
-  for (let i = 1; i < obj.length; i++) {
-    if (firstId !== obj[i].userId) {
-      key = obj[i].userId;
-      json[key] = parseInt(count(obj[i].userId));
-      firstId = obj[i].userId;
-    }
-  }
-  console.log(json);
+    const obj = JSON.parse(body);
+    let json = {};
+    obj.forEach((ob) => {
+      if (ob.completed && json[ob.userId] === undefined) {
+        json[ob.userId] = 1;
+      } else if (ob.completed) {
+        json[ob.userId] += 1;
+      }
+    });
+    console.log(json); 
 });
