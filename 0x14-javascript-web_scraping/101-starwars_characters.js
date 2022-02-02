6895ee2a56525476231089ac4912426bdc067d70
Daniel-bee <daniel123_86@live.com>
2022-02-02 14:50:58 +0300

    starwars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,23 +2,31 @@
 
 const request = require('request');
 const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
-  method: 'GET'
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2]
 };
 
 request(options, (error, response, body) => {
   if (error) console.log(error);
   const obj = JSON.parse(body);
+  /*
   function order (url) {
     request(url, (error, response, body) => {
       if (error) console.log(error);
       const people = JSON.parse(body);
-      console.log(people.name);
+      console.log(people.url);
     });
   }
-  let url = "";
+  */
   for (let i = 0; i < obj.characters.length; i++) {
-    url = obj.characters[i];
-    order(url);
+    request.get(obj.characters[i], (error, response, body) => {
+      if (error) console.log(error);
+      const people = JSON.parse(body);
+      const arr = [];
+      arr.push(people);
+      arr.sort(function (a, b) {
+        return a.obj.characters[i] - b.people.url;
+      });
+      console.log(arr[0].name);
+    });
   }
 });
