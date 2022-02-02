cd168e9663b3c24864de3771ca1bc90411ba2475
Daniel-bee <daniel123_86@live.com>
2022-02-02 08:11:26 +0300

    star wars characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,14 +2,18 @@
 
 const request = require('request');
 const options = {
-  url: process.argv[2]
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  method: 'GET'
 };
+
 request(options, (error, response, body) => {
-  if (error) throw error;
-  for (const character of JSON.parse(body).characters) {
-    request(character, function (error, response, body) {
-      if (error) throw error;
-      console.log(JSON.parse(body).name);
+  if (error) console.log(error);
+  const obj = JSON.parse(body);
+  for (let i = 0; i < obj.characters.length; i++) {
+    request(obj.characters[i], (error, response, body) => {
+      if (error) console.log(error);
+      const people = JSON.parse(body);
+      console.log(people.name);
     });
   }
 });
