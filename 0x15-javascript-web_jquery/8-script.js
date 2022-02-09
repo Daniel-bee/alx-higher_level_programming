dced0ddae5c3972a318440c10ae3545aae880cd7
Daniel-bee <daniel123_86@live.com>
2022-02-09 15:17:56 +0300

    Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,14 @@
-const request = require('request');
-const options = {
-  url: 'https://swapi.co/api/films/?format=json',
-  method: 'GET'
-};
+$('document').ready(function () {
+  const request = require('request');
+  const options = {
+    url: 'https://swapi.co/api/films/?format=json',
+    method: 'GET'
+  };
 
-request(options, function (response) {
-  for (const value of response.results) {
-    const item = $('<li></li>').text(value.title);
-    $('#list_movies').append(item);
-  }
+  request(options, function (response) {
+    for (const value of response.results) {
+      const item = $('<li></li>').text(value.title);
+      $('#list_movies').append(item);
+    }
+  });
 });
